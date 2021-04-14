0c5a03e776902513eddd63c58c127d28194117a9
Michael Huskey <40217812+mhusk@users.noreply.github.com>
2021-04-13 21:44:57 -0400

    Update VerifyingAndMovingNewResponses.js

--- VerifyingAndMovingNewResponses.js
+++ VerifyingAndMovingNewResponses.js
@@ -1,13 +1,6 @@
-var ss = GetSpreadsheet(spreadsheetID);
-var response_SHEET = GetSheet('Form Responses',ss);
-var globalVars_SHEET = GetSheet('globalVars', ss);
-var textingServiceMembers_SHEET = GetSheet('TextingServiceMembers', ss);
-
-
-
-function main(){
-  ValidateFormResponses(response_SHEET, globalVars_SHEET); //Update this function to not have to rely on global variables. Make it rely on looking through the responses for unverified rows.
-  MoveVerifiedResponses(response_SHEET, textingServiceMembers_SHEET);
+function VerifyAndMove(){
+  ValidateFormResponses(response_SHEET);
+  MoveVerifiedResponses(response_SHEET, members_SHEET);
 }
 
 
@@ -80,9 +73,8 @@ function GetRowOfLastMoved(formResponseSheet){
 /**
  * This will validate new form responses
  * @param {SpreadsheetApp.Spreadsheet} formResponses - sheet with the form responses
- * @param {SpreadsheetApp.Spreadsheet} globalVars - sheet with global variables
  */
-function ValidateFormResponses(formResponses, globalVars){
+function ValidateFormResponses(formResponses){
   var status_COL = 4;
   var phoneNumber_COL = 3;
 
@@ -129,30 +121,7 @@ function GetNewResponses(formResponseSheet){
   return rows;
 }
 
-/**
- * This will get the sheet you want based on name
- * @param {string} name - this is the name of the sheet you want
- * @param {SpreadsheetApp.Spreadsheet} spreadsheet - this is the spreadsheet you want to get the sheet from
- */
-function GetSheet(name, spreadsheet){
-  var sheet = spreadsheet.getSheetByName(name);
-  if(sheet != null){
-    return sheet;
-  } else{
-    Logger.log('The sheet with the name: ' + name +' does not exist');
-  }
-}
 
 
-/**
- * This will return a Google Sheet based on the sheet's ID
- * @param {string} ID - this is the ID for the sheet
- */
-function GetSpreadsheet(ID){
-  try {
-    var spreadsheet = SpreadsheetApp.openById(ID);
-    return spreadsheet;
-  } catch (error) {
-    Logger.log('Not a valid Spreadsheet ID');
-  }
-}
+
+
